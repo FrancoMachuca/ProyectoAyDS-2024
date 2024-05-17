8725d0cb2d1875ae7ce5ab2b3e7f13818db79592
IgnaCerutti <zirouser@gmail.com>
2024-05-17 02:16:32 -0300

    Logica de registro usuario

--- myapp.rb
+++ myapp.rb
@@ -13,10 +13,19 @@ get '/registro' do
     File.read('registro.html')
 
 end
-
-post '/login/post' do
-
-
+post '/registro' do
+    user = User.new(params[:user][:password])
+    user.save
+    redirect '/login'
+end
+post '/login' do
+    user = User.find_by(email: params[:user][:email])
+    if user && user.password == params[:user][:password]
+        session[:user_id] = user.id
+        redirect '/home'
+        else
+            redirect '/login'
+            end
 end 
 
 
