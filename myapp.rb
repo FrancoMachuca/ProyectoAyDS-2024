03401c8e8377dcc4ba8747b6774a9492380b470c
IgnaCerutti <zirouser@gmail.com>
2024-05-29 04:50:19 -0300

    Registro e inicio de sesion hechos

--- myapp.rb
+++ myapp.rb
@@ -8,11 +8,13 @@ require './models/question'
 
 
 
+
 class MyApp < Sinatra::Application
     def initialize(myapp = nil)
         super()
     end
     set :database_file, './config/database.yml'
+    enable :sessions
 
     get '/' do
         redirect '/login'
@@ -21,30 +23,44 @@ class MyApp < Sinatra::Application
     get '/login' do
         erb :login
     end
+
+    post '/login' do
+        user = User.find_by(name: params[:name], password: params[:password])
+        if user
+            session[:user_id] = user.id
+            redirect '/menu'
+        else
+            @error_message = "Nombre de usuario o contraseña son incorrectas"
+            erb :login
+        end
+    end 
     
     get '/registro' do
         erb :registro
     end
+    
     post '/registro' do
-        user = User.new(params[:user][:password])
-        user2 = User.find_by(email: params[:user][:email])
-        if user==user2
-            @message = "Usted ya tenia una cuenta previa"
-            redirect '/login'
+        user = User.find_by(mail: params[:mail]) || User.find_by(name: params[:name])
+        if user
+            @error_message = "Usted ya tenia una cuenta previa"
+            erb :login
         else
-            user.save
-            redirect '/login'
+            user = User.new(name: params[:name], mail: params[:mail],password: params[:password], totalScore: 0)
+            if user.save
+                session[:user_id] = user.id
+                redirect '/login'
+            else
+                @error_message = "Error al crear cuenta"
+                erb :registro
+            end
         end
     end
-    post '/login' do
-        user = User.find_by(email: params[:user][:email])
-        if user && user.password == params[:user][:password]
-            session[:user_id] = user.id
-            redirect '/home'
-        else
-            redirect '/login'
-        end
-    end 
+    
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
+
 
 end
 
