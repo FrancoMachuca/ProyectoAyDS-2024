4825c1b5e490132f4553ccdbf2fa8ed3e90ab5af
Hastro <valenatali0347@gmail.com>
2024-05-30 23:42:01 -0300

    No funciona nada

---
+++ db/migrate/20240515221608_create_multiple_choices_table.rb
@@ -0,0 +1,12 @@
+class CreateMultipleChoicesTable < ActiveRecord::Migration[7.1]
+  def change
+    create_table :multiple_choices do |u|
+      u.integer :number_answer
+      u.references :question, foreign_key: true
+      u.string :type
+
+      u.datetime :created_at
+      u.datetime :updated_at
+    end
+  end
+end
--- myapp.rb
+++ myapp.rb
@@ -6,14 +6,14 @@ require './models/lesson'
 require './models/exam'
 require './models/question'
 require './models/user_level'
-
-
-
+require './models/multiple_choice'
+require './models/multiple_choice_answer'
 
 class MyApp < Sinatra::Application
     def initialize(myapp = nil)
         super()
     end
+
     set :database_file, './config/database.yml'
     enable :sessions
 
@@ -45,14 +45,13 @@ class MyApp < Sinatra::Application
         erb :registro
     end
 
-
     post '/registro' do
         user = User.find_by(mail: params[:mail]) || User.find_by(name: params[:name])
         if user
-            @error_message = "Usted ya tenia una cuenta previa"
+            @error_message = "Usted ya tenía una cuenta previa"
             erb :login
         else
-            user = User.new(name: params[:name], mail: params[:mail],password: params[:password], totalScore: 0)
+            user = User.new(name: params[:name], mail: params[:mail], password: params[:password], totalScore: 0)
             if user.save
                 session[:user_id] = user.id
                 redirect '/login'
@@ -87,18 +86,69 @@ class MyApp < Sinatra::Application
     end
 
     get '/jugar' do
-        @user = User.find(session[:user_id])
-        @levels = Level.all.order(:id)
-        erb :jugar
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+            @levels = Level.all.order(:id)
+            erb :jugar
+        else
+            redirect '/login'
+        end
     end
 
     get '/ranking' do
         if session[:user_id]
-          @users = User.order(totalScore: :desc)
-          erb :ranking
+            @users = User.order(totalScore: :desc)
+            erb :ranking
         else
-          redirect '/login'
+            redirect '/login'
         end
     end
 
+    get '/level/:id' do
+        if session[:user_id]
+            @level = Level.find(params[:id])
+            @questions = @level.questions
+            erb :level
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/question/:id' do
+        if session[:user_id]
+            @question = Question.find(params[:id])
+            case @question
+            when MultipleChoice
+                @answers = @question.multiple_choice_answers
+                erb :multiple_choice
+            else
+                erb :unknown_question_type
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/question/:id/check' do
+        if session[:user_id]
+            @question = Question.find(params[:id])
+            correct = false
+
+            case @question
+            when MultipleChoice
+                answer = MultipleChoiceAnswer.find(params[:answer_id])
+                correct = answer.correct
+            end
+
+            if correct
+                # Lógica para cuando la respuesta es correcta
+                redirect "/level/#{@question.level_id}", notice: '¡Correcto!'
+            else
+                # Lógica para cuando la respuesta es incorrecta
+                redirect "/question/#{@question.id}", alert: 'Respuesta incorrecta. Inténtalo de nuevo.'
+            end
+        else
+            redirect '/login'
+        end
+    end
 end
