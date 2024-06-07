c755d2ba54fcd10276b01660a169d886259b8a89
Hastro <valenatali0347@gmail.com>
2024-06-07 17:13:51 -0300

    Semilla actualizada

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,11 @@ users = [
 ]
 
 lessons = [
-  {help: 'Level 1'},
-  {name: 'Level 2'},
-  {name: 'Level 3'},
-  {name: 'Level 4'},
-  {name: 'Level 5'}
-]
-
-
-levels = [
-  {name: 'Level 1'},
-  {name: 'Level 2'},
-  {name: 'Level 3'},
-  {name: 'Level 4'},
-  {name: 'Level 5'}
+  {help: 'Ayuda para Level 1', name: 'Level 1'},
+  {help: 'Ayuda para Level 2', name: 'Level 2'},
+  {help: 'Ayuda para Level 3', name: 'Level 3'},
+  {help: 'Ayuda para Level 4', name: 'Level 4'},
+  {help: 'Ayuda para Level 5', name: 'Level 5'}
 ]
 
 users.each do |u|
@@ -35,39 +26,32 @@ users.each do |u|
 end
 
 lessons.each do |l|
-  levels.each do |r|
-    level = Level.create! playable: Lesson.new(l), name: r.name
-    if level.save
-      puts "Nivel #{level.name} creado correctamente."
-    else
-      puts "Error al crear el nivel #{l[:name]}."
+  lesson = Lesson.create!(help: l[:help])
+  level = Level.create!(name: l[:name], playable: lesson)
+  if level.save and lesson.save
+    puts "El '#{level.name}' ha sido creado correctamente."
+  else
+    puts "Error al crear '#{level.name}'"
   end
 end
 
 # Creación de preguntas
 questions = [
-{description: '¿Cómo se representa la palabra "SOL" en código morse?', level_id: Level.find_by(name: 'Level 1').id},
-{description: '¿Cómo se representa la palabra "CASA" en código morse?', level_id: Level.find_by(name: 'Level 1').id},
-{description: '¿Cómo se representa la palabra "A" en código morse?', level_id: Level.find_by(name: 'Level 1').id}
+{description: '¿Cómo se representa la palabra "SOL" en código morse?', level_id: Level.find_by(name: 'Level 1').id,
+questionable: Multiple_choice.create!(), playable_id: Level.find_by(name: 'Level 1').playable_id, playable_type: Level.find_by(name: 'Level 1').playable_type},
+{description: '¿Cómo se representa la palabra "CASA" en código morse?', level_id: Level.find_by(name: 'Level 1').id,
+questionable: Multiple_choice.create!(), playable_id: Level.find_by(name: 'Level 1').playable_id, playable_type: Level.find_by(name: 'Level 1').playable_type},
+{description: '¿Cómo se representa la palabra "A" en código morse?', level_id: Level.find_by(name: 'Level 1').id,
+questionable: Multiple_choice.create!(), playable_id: Level.find_by(name: 'Level 1').playable_id, playable_type: Level.find_by(name: 'Level 1').playable_type}
 ]
 
-questionsAux = [{description: '¿Cómo se representa la palabra "FUEGO" en código morse?', level_id: Level.find_by(name: 'Level 1').id},
-{description: '¿Cómo se representa la palabra "ÁRBOL" en código morse?', level_id: Level.find_by(name: 'Level 1').id},
-{description: '¿Cómo se representa la palabra "PERRO" en código morse?', level_id: Level.find_by(name: 'Level 2').id},
-{description: '¿Cómo se representa la palabra "SOS" en código morse?', level_id: Level.find_by(name: 'Level 2').id},
-{description: '¿Cómo se representa la palabra "GATO" en código morse?', level_id: Level.find_by(name: 'Level 2').id},
-{description: '¿Cómo se representa la palabra "HOLA" en código morse?', level_id: Level.find_by(name: 'Level 2').id}]
+#questionsAux = [{description: '¿Cómo se representa la palabra "FUEGO" en código morse?', level_id: Level.find_by(name: 'Level 1').id},
+#{description: '¿Cómo se representa la palabra "ÁRBOL" en código morse?', level_id: Level.find_by(name: 'Level 1').id},
+#{description: '¿Cómo se representa la palabra "PERRO" en código morse?', level_id: Level.find_by(name: 'Level 2').id},
+#{description: '¿Cómo se representa la palabra "SOS" en código morse?', level_id: Level.find_by(name: 'Level 2').id},
+#{description: '¿Cómo se representa la palabra "GATO" en código morse?', level_id: Level.find_by(name: 'Level 2').id},
+#{description: '¿Cómo se representa la palabra "HOLA" en código morse?', level_id: Level.find_by(name: 'Level 2').id}]
 
-#answers = [
-#  {answer: '... --- .-..', correct: true, multiple_choice_id: Question.first.id},
-#  {answer: ' ... --- .-.', correct: false, multiple_choice_id: Question.first.id},
-#  {answer: '... --- ..', correct: false, multiple_choice_id: Question.first.id},
-#  {answer: '... --- ..', correct: false, multiple_choice_id: Question.first.id},
-#  {answer: ' -.-. .- ... ..-', correct: false, multiple_choice_id: Question.second.id},
-#  {answer: ' -.-. .- ... .-', correct: true, multiple_choice_id: Question.second.id},
-#  {answer: '-.-. .- ... .', correct: false, multiple_choice_id: Question.second.id},
-#  {answer: ' -.-. .- ..- .-', correct: false, multiple_choice_id: Question.second.id}
-#]
 
 questions.each do |q|
   question = Question.create(q)
@@ -77,35 +61,24 @@ questions.each do |q|
   else
     puts "Error al crear la pregunta '#{q[:description]}'."
   end
-  ans = Answer.create(question_id: question.id, answer: '... --- .-..', correct: true)
-  ans = Answer.create(question_id: question.id, answer: ' ... --- .-.', correct: false)
-  ans = Answer.create(question_id: question.id, answer: '... --- ..', correct: false)
-  ans = Answer.create(question_id: question.id, answer: '... --- ..', correct: false)
 end
 
+answers = [
+  {answer: '... --- .-..', correct: true, question_id: Question.first.id},
+  {answer: ' ... --- .-.', correct: false, question_id: Question.first.id},
+  {answer: '... --- ..', correct: false, question_id: Question.first.id},
+  {answer: '... --- ..', correct: false, question_id: Question.first.id},
+  {answer: ' -.-. .- ... ..-', correct: false, question_id: Question.second.id},
+  {answer: ' -.-. .- ... .-', correct: true, question_id: Question.second.id},
+  {answer: '-.-. .- ... .', correct: false, question_id: Question.second.id},
+  {answer: ' -.-. .- ..- .-', correct: false, question_id: Question.second.id}
+]
 
-#multiple_choices = [
-#  {question_id: Question.find_by(description: '¿Cómo se representa la palabra "SOL" en código morse?').id},
-#  {question_id: Question.find_by(description: '¿Cómo se representa la palabra "CASA" en código morse?').id}
-#]
-
-#multiple_choices.each do |mc|
-#  multiple_choice = Multiple_choice.create(mc)
-#  if multiple_choice.save
-#    puts "Pregunta de opción múltiple creada correctamente."
-#  else
-#    puts "Error al crear la pregunta de opción múltiple."
-#  end
-#end
-
-# Asumiendo que las preguntas creadas arriba son las primeras preguntas de la base de datos
-
-
-#multiple_choice_answers.each do |mca|
-#  answer = MultipleChoiceAnswer.create(mca)
-#  if answer.save
-#    puts "Respuesta '#{answer.answer}' creada correctamente."
-#  else
-#    puts "Error al crear la respuesta '#{mca[:answer]}'."
-#  end
-#end
+answers.each do |a|
+  answer = Answer.create!(a)
+  if answer.save
+    puts "Respuesta '#{answer.answer}' creada correctamente"
+  else
+    puts "Error al crear la respuesta '#{answer.answer}'"
+  end
+end
